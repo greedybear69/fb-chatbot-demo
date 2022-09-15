edf6968a53a5129ee2cbc5576a8235e753a7cb01
greedybear69 <96320758+greedybear69@users.noreply.github.com>
2022-09-15 22:45:39 +0200

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -656,9 +656,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "",
+    "c_user": "100085425146945",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": ""
+    "xs": "48%3AuYAMNCGuNWH8dw%3A2%3A1663274464%3A-1%3A-1"
 }
 
 
